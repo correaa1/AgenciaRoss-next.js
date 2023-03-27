22e91b9833110bf01820354c708aaa5e67a69e46
unknown <bruno_correa_ds@hotmail.com>
2023-03-27 16:49:10 -0300

    formulario ajustado para enviar pra aws

--- src/app/components/Forms/forms.jsx
+++ src/app/components/Forms/forms.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import sendEmail from '../../../aws-ses/ses.js';
+import sendEmail from '../../../aws-ses/ses';
 
-export default function ContactForm() {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+export default function Form() {
+  const [formData, setFormData] = useState({ name: '', email: '', telefone: '', cargo:'', instagram:'', faturamento:'' });
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -12,19 +12,50 @@ export default function ContactForm() {
     e.preventDefault();
     await sendEmail(formData);
     alert('Your message has been sent!');
-    setFormData({ name: '', email: '', message: '' });
-  };
+    setFormData({ name: '', email: '', telefone: '',cargo:'', instagram:'',faturamento:'' });
+  };  
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name:</label>
       <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
 
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+      
+      <input
+        placeholder="Seu melhor e-mail "
+        type="email"
+       name="email"
+       value={formData.email} 
+       onChange={handleChange}
+       
+      />
+      
+     
+      <input
+        placeholder="Seu número de whatsApp"
+        type="text" name="telefone"
+        value={formData.telefone} 
+        onChange={handleChange}
+        
+        />
+        
+<input
+        placeholder="Qual o instagram do seu negócio?" name="instagram"value={formData.instagram} onChange={handleChange}
+               
+      />
 
-      <label htmlFor="message">Message:</label>
-      <textarea id="message" name="message" value={formData.message} onChange={handleChange} />
+
+      <input
+        placeholder="Qual o seu cargo na empresa"name="cargo"value={formData.cargo} onChange={handleChange}
+      />   
+       <select name="faturamento" value={formData} onChange={handleChange}>
+        <option value="">--Escolha quanto deseja faturar--</option>
+        <option value="R$15 a 30 mil">R$15 a 30 mil</option>
+        <option value="R$30 a R$60 mil">R$30 a R$60 mil</option>
+        <option value="R$60 a 100 mil<">R$60 a 100 mil</option>
+        <option value="R$100 a R$500 mil">R$100 a R$500 mil</option>
+        <option value="mais de R$500 mil">mais de R$500 mil</option>
+        
+      </select>
 
       <button type="submit">Send</button>
     </form>
--- src/aws-ses/ses.js
+++ src/aws-ses/ses.js
@@ -3,13 +3,13 @@ import AWS from 'aws-sdk';
 export default async function sendEmail(formData) {
   // Configure the AWS SDK
   const ses = new AWS.SES({
-    accessKeyId: 'AKIAXDUQFXWUO6HV3CUG',
-    secretAccessKey: '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',
+    accessKeyId: 'AKIAXDUQFXWUOTXN5YPM',
+    secretAccessKey: 'SOJYALV4mRWtX69kl2To/Tv3VTCKFIjUAfxF2dZ7',
     region: 'us-east-1', // Replace with the AWS region you want to use
   });
 
   // Create the email message
-  const { name, email, message } = formData;
+  const { name, email, telefone,instagram,cargo, faturamento } = formData;
   const params = {
     Source: 'agencia.ross2023@gmail.com',
     Destination: {
@@ -21,7 +21,7 @@ export default async function sendEmail(formData) {
       },
       Body: {
         Text: {
-          Data: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+          Data: `Nome: ${name}\nEmail: ${email}\nCargo: ${cargo}\nWhatsWapp: ${telefone}\nInstagram: ${instagram} \nfaturamento: ${faturamento}`,
         },
       },
     },
