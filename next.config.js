390334d729bbbface1e48cc13bce1e44bfe443dc
unknown <bruno_correa_ds@hotmail.com>
2023-03-30 10:58:31 -0300

    env configurado no next.config .gitignore

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+require('dotenv').config()
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  
+  },
+  env: {      
+  AWS_ACCESS_KEY_ID:'AKIAXDUQFXWUIOQM5SWP',      
+  AWS_SECRET_ACCESS_KEY:'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',  
+  AWS_REGION: 'us-east-1',
+      SES_EMAIL: 'agencia.ross2023@gmail.com',
+    },
+  };
+
+module.exports = nextConfig
